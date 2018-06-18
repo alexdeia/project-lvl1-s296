fb991a479dd9d72dd06be9188d2ebe663ab9a659
Alexey Klykov <alexk.deia@gmail.com>
2018-06-18 15:54:49 +0300

    [feature] step-2. Add a greeting to the player by CLI

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync'
+
+console.log('Welcome to the Brain Games!', '\n')
+
+const playerName = readlineSync.question('May I have your name? ')
+console.log(`Hello, ${playerName}!`)
