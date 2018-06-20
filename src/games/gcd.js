a9dc732b9ec3c087ec7845b954a8fe5ea99d628a
Alexey Klykov <alexk.deia@gmail.com>
2018-06-20 14:51:51 +0300

    [feature] add brain-gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,20 @@
+import { getRandomNumber, runGame } from '../common-function';
+
+const getTask = () => 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (num1, num2) => {
+  if (num2 === 0) {
+    return Math.abs(num1);
+  }
+  return getGcd(num2, num1 % num2);
+};
+
+const getData = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const rightAnswer = getGcd(num1, num2);
+  const question = `Question: ${num1} ${num2}`;
+  return { rightAnswer, question };
+};
+
+export default () => runGame(getTask, getData);
